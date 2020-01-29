30bed8e05897466f0b1c3a2664683b16ffe4c281
NefiteTifall <nefitetifall@gmail.com>
2020-01-29 17:34:39 +0100

    no message

--- Login/JS/index.js
+++ Login/JS/index.js
@@ -1,3 +1,23 @@
-document.body.getElementById('motDePasse').addEventListener('change', function (event) {
-    console.log("");
+var mdp;
+var nb = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var checkNb;
+var verif = 0;
+document.getElementById('motDePasse').addEventListener('keyup', function(event) {
+    var verif = 0;
+    mdp = document.getElementById('motDePasse').value;
+    if (mdp.length >= 6) {
+        verif += 25;
+    }
+    if (mdp.length >= 12) {
+        verif += 25;
+    }
+    for (var i = 0; i < mdp.length; i++) {
+        if (nb.indexOf(mdp[i]) != -1) {
+            checkNb = true;
+        }
+    }
+    if (checkNb == true) {
+        verif += 25;
+    }
+    console.log(verif);
 });
