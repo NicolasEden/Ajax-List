1f5e49691d13001094c01e2725fa8ab66e74640b
Florian <fdupin@edenschool.fr>
2020-02-04 15:06:39 +0100

    HTML/CSS Login fini a 100%

--- Login/JS/darkmodRegister.js
+++ Login/JS/darkmodRegister.js
@@ -1,8 +1,8 @@
 function darkmod() {
     
     if (document.getElementById("customSwitch1").checked) {
-        document.getElementById("toto").setAttribute("href","../Login/SCSS/dark.css")
+        document.getElementById("toto").setAttribute("href","../Login/SCSS/dark-register.css")
     }else {
-        document.getElementById("toto").setAttribute("href","../Login/SCSS/light.css")
+        document.getElementById("toto").setAttribute("href","../Login/SCSS/light-register.css")
     }
 }
