824f5ddd2f5b667c31e4f7ea5698b9fc5db5e116
NefiteTifall <nefitetifall@gmail.com>
2020-02-04 14:28:26 +0100

    no message

--- Login/JS/index.js
+++ Login/JS/index.js
@@ -1,25 +1,45 @@
 var mdp;
-var nb = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var nb = ["", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 var checkNb;
+var checkUC;
 var verif = 0;
 document.getElementById('motDePasse').addEventListener('keyup', function(event) {
+    var checkNb;
+    var checkUC;
     var verif = 0;
     mdp = document.getElementById('motDePasse').value;
-    if (mdp.length >= 6) {
+    if (mdp.length >= 8) {
         verif += 25;
     }
-    if (mdp.length >= 12) {
+    if (format.test(mdp)) {
         verif += 25;
     }
-    for (var i = 0; i < mdp.length; i++) {
-        if (nb.indexOf(mdp[i]) != -1) {
-            checkNb = true;
+    if (mdp.length >= 1) {
+        for (var i = 0; i < mdp.length; i++) {
+            var testL = mdp[i]
+            if (nb.indexOf(mdp[i]) != -1) {
+                checkNb = true;
+            } else if (checkNb != true){
+                checkNb = false;
+            }
+            if (nb.indexOf(mdp[i]) == -1) {
+                if (format.test(mdp[i]) == false) {
+                    if (mdp[i] == mdp[i].toUpperCase()) {
+                        checkUC = true;
+                    } else if (checkUC != true) {
+                        checkUC = false;
+                    }
+                }
+            }
+        }
+        if (checkNb == true) {
+            verif += 25;
+        }
+        if (checkUC == true) {
+            verif += 25;
         }
     }
-    if (checkNb == true) {
-        verif += 25;
-    }
-    console.log(verif);
 });
 
 
